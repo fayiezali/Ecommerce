a64a284b711507e70f9216fa45ecd303278511ed
Fayiez Ali <39649391+fayiezali@users.noreply.github.com>
2023-05-12 16:42:19 -0400

    Add files via upload
    Category & Dashboard & Forgot_Password & Login_Logout

--- category/models.py
+++ category/models.py
@@ -9,7 +9,7 @@ class CategoryMODEL(models.Model):
 	
 	Cate_name         = models.CharField(max_length=200      , db_index=True   ,                         verbose_name="Name")
 	Cate_slug          = models.SlugField(max_length=200       , db_index=True  , unique=True ,            verbose_name="Slug")
-	Cate_category_image = models.ImageField(upload_to="Catgory_File_Photo/" , db_index=True , blank=False  , null=False , verbose_name="Image Preview"  ,default='Default_Image.png')
+	Cate_image        = models.ImageField(upload_to="Catgory_File_Photo/" , db_index=True , blank=False  , null=False , verbose_name="Image Preview"  ,default='Default_Image.png')
 	Cate_available 	    = models.BooleanField(default=True       , db_index=True ,                         verbose_name="Available")
 #
 	class Meta:
@@ -33,7 +33,7 @@ class SubCategoryMODEL(models.Model):
 	SubCat_category       = models.ForeignKey(CategoryMODEL , related_name='categories_sub' , on_delete=models.CASCADE , verbose_name="Category")
 	SubCat_name          = models.CharField(max_length=200 , db_index=True                                        , verbose_name="Name")
 	SubCat_slug           = models.SlugField(max_length=200 , db_index=True,unique=True                             , verbose_name="Slug")
-	SubCat_category_image  = models.ImageField(upload_to="Sub_Catgories_File_Photo/" , db_index=True , blank=False  , null=False , verbose_name="Image Preview"  ,default='Default_Image.png')
+	SubCat_image         = models.ImageField(upload_to="Sub_Catgories_File_Photo/" , db_index=True , blank=False  , null=False , verbose_name="Image Preview"  ,default='Default_Image.png')
 	SubCat_available 	   = models.BooleanField(default=True       , db_index=True                                  , verbose_name="Available")
 
 	class Meta:
@@ -46,6 +46,27 @@ class SubCategoryMODEL(models.Model):
     # 'admin'display the field name on a page
 	def __str__(self):
 		return self.SubCat_name
+#
+#
+#
+# Module Sub Category Branch
+class SubCategoryBranchMODEL(models.Model):
+	SubCatBra_category        = models.ForeignKey(SubCategoryMODEL , related_name='sub_category_branch' , on_delete=models.CASCADE , verbose_name="Sub Category Branch") 
+	SubCatBra_name           = models.CharField(max_length=200 , db_index=True                                        , verbose_name="Name")
+	SubCatBra_slug            = models.SlugField(max_length=200 , db_index=True,unique=True                             , verbose_name="Slug")
+	SubCatBra_image          = models.ImageField(upload_to="Sub_Catgories_Branch_File_Photo/" , db_index=True , blank=False  , null=False , verbose_name="Image Preview"  ,default='Default_Image.png')
+	SubCatBra_available 	   = models.BooleanField(default=True       , db_index=True                                  , verbose_name="Available")
+
+	class Meta:
+	    # 'Z-A' in descending order
+		ordering = ('SubCatBra_name',)
+		verbose_name = "Sub Category Branch"
+		# The Name of the Model That Will Be Displayed In The Admin Page
+		verbose_name_plural = "Sub Categories Branch"
+
+    # 'admin'display the field name on a page
+	def __str__(self):
+		return self.SubCatBra_name
 #
 #
 #
