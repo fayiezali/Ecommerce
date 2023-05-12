42167c990c0d679b0fffa0ff4042902e70e85e3c
Fayiez Ali <39649391+fayiezali@users.noreply.github.com>
2023-05-12 16:53:54 -0400

    Add files via upload
    04_static & store & Templates & TRASH

---
+++ store/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render
+from .models import Cartitems, Customer, Product, Cart
+from django.http import JsonResponse
+import json
+
+# Create your views here.
+def store(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        created = None
+        cart, created = Cart.objects.get_or_create(customer = customer, completed = False)
+        # cart,  Cart.objects.get_or_create(customer = customer, completed = False)
+        cartitems = cart.cartitems_set.all()
+    products = Product.objects.all()
+    return render(request, 'store/store.html', {'products': products, 'cart':cart})
+
+
+def cart(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        # cart,  Cart.objects.get_or_create(customer = customer, completed = False)
+        cart, created = Cart.objects.get_or_create(customer = customer, completed = False)
+        cartitems = cart.cartitems_set.all()
+    else:
+        cartitems = []
+        cart = {"get_cart_total": 0, "get_itemtotal": 0}
+
+    return render(request, 'store/cart.html', {'cartitems' : cartitems, 'cart':cart})
+
+
+# def checkout(request):
+#     return render(request, 'checkout.html', {})
+
+def updateCart(request):
+    data = json.loads(request.body)
+    productId = data["productId"]
+    action = data["action"]
+    product = Product.objects.get(id=productId)
+    customer = request.user.customer
+    # created = None
+    cart, created = Cart.objects.get_or_create(customer = customer, completed = False)
+    cartitem, created = Cartitems.objects.get_or_create(cart = cart, product = product)
+    if action == "add":
+        print('Check def udateCart')
+        cartitem.quantity += 1
+        cartitem.save()
+    return JsonResponse("Cart Updated", safe = False)
+
+
+def updateQuantity(request):
+    data = json.loads(request.body)
+    quantityFieldValue = data['qfv']
+    quantityFieldProduct = data['qfp']
+    product = Cartitems.objects.filter(product__name = quantityFieldProduct).last()
+    product.quantity = quantityFieldValue
+    product.save()
+    return JsonResponse("Quantity updated", safe = False)
